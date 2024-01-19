8db49d8cfea40dd81ce9e5afaa95b9adca79e166
arisurya7 <arisurya425@gmail.com>
2024-01-19 15:10:13 +0800

    change store plant desease

--- app/Http/Controllers/API/PlantDiseaseController.php
+++ app/Http/Controllers/API/PlantDiseaseController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\PlantDiseaseRequest;
 use App\Models\PlantDisease;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class PlantDiseaseController extends Controller
@@ -27,17 +28,19 @@ class PlantDiseaseController extends Controller
 
     public function store(PlantDiseaseRequest $request) {
         try {
+            DB::beginTransaction();
             $filenameImg = time().'.'.$request->img->extension();
             $request->file('img')->move(public_path('media/public'), $filenameImg);
 
             PlantDisease::create([
                 'name' => $request->name,
-                'img' => asset("media/image".$filenameImg.""),
+                'img' => asset("media/image/".$filenameImg.""),
                 'desc' => $request->desc
             ]);
-            
-            return response()->json(['status' => true, 'message' => 'data success created']);   
+            DB::commit();
+            return response()->json(['status' => true, 'message' => 'data success created'], 201);   
         } catch (Exception $e) {
+            DB::rollBack();
             Log::error('Error store plant desease : ' . $e->getMessage());
             return response()->json(['status' => false, 'message' => $e->getMessage()], 400);
         }
